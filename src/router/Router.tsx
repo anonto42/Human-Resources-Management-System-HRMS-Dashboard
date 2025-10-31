baba005bdcf566a6144ce4830dfa1ec8649e8e9d
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-31 19:06:23 +0600

    Main Routing element added

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,27 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "../pages/dashboard/Home.tsx";
+import SignIn from "../pages/auth/SignIn.tsx";
+import NotFoundPage from "../pages/not-found/NotFoundPage.tsx";
+import ProtectedRoute from "./ProtectedRoute.tsx";
 
-export default ()=> (
-    <Router>
-        <Routes>
-            <Route path="/" element={<HomePage/>}></Route>
-        </Routes>
-    </Router>
-)
+export default function RouterElement () {
+
+    return (
+        <Router>
+            <Routes>
+0
+                {/* Auth Routes */}
+                <Route path="/sign-in" element={<SignIn />} />
+
+                {/* Dashboard Routes */}
+                <Route element={<ProtectedRoute />} >
+                    <Route  path="/"  element={<HomePage />} />
+                </Route>
+
+                {/* Not Found */}
+                <Route path="*" element={<NotFoundPage />} />
+
+            </Routes>
+        </Router>
+    )
+}
