c89c6ecce1792d4d652cfc3593e590d5babeb027
Sohidul Islam Ananto <anontom90@gmail.com>
2025-10-31 20:02:47 +0600

    Auth layout

--- src/layout/AuthLayout.tsx
+++ src/layout/AuthLayout.tsx
@@ -1,4 +1,18 @@
+import type {ReactNode} from "react";
 
-export default () => {
+export default function AuthLayout ({ children }: { children: ReactNode }) {
+    return (
+        <main className={"w-full min-h-svh relative flex justify-center items-center bg-white overflow-hidden"}>
+
+            {/* Round Elements */}
+            <div className={"absolute -top-[50vh] -left-[60vh] xl:-left-[40vh] xl:-top-[70vh] w-[120vh] xl:w-[150vh] h-[120vh] xl:h-[150vh] rounded-full bg-gradient-to-r from-[#002282] to-[#0170DA]"} />
+            <div className={"absolute -bottom-[50vh] -left-[50vh] w-[90vh] h-[90vh] xl:-bottom-[40vh] xl:-left-[40vh] rounded-full bg-gradient-to-r from-[#4A58F9] to-[#8BEAFE] shadow-md"} />
+            <div className={"absolute top-[50vh] -right-[40vh] w-[80vh] h-[80vh] rounded-full bg-gradient-to-r to-[#4A58F9] from-[#8BEAFE] shadow-md"} />
+
+            {/* Passed the Children props */}
+            <div className="absolute w-full h-full">{ children }</div>
+
+        </main>
+    )
 
 }
