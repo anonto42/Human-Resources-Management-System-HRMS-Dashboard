67047e95c42638044b02cddc4585cfa5c97cd142
Sohidul Islam Ananto <anontom90@gmail.com>
2025-11-01 10:09:18 +0600

    toast container added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import Router from "./router/Router.tsx";
 import {Provider} from "react-redux";
 import {store} from "./redux/stores/store.ts";
+import {Toaster} from "sonner";
 
 export default function App () {
     return (
         <Provider store={store}>
             <Router />
+            <Toaster />
         </Provider>
     )
 }
